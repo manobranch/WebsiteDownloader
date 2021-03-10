bf3b1b1853811043ae9010bba816b61932377aa1
Martin Nordkvist <martin.nordkvist@sogeti.com>
2021-03-10 21:31:55 +0100

    Basic startup structure

---
+++ WebsiteDownloaderProgram/WebsiteDownloaderProgram/Downloader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsiteDownloaderProgram
+{
+    public class Downloader
+    {
+        #region Startup region
+
+        public Downloader()
+        {
+
+        }
+
+        public void Run(string webSite)
+        {
+            ToScreen($"Downloading website: {webSite}");
+        }
+
+        #endregion
+
+        #region Utility methods
+
+        private void ToScreen(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        #endregion
+    }
+}
--- WebsiteDownloaderProgram/WebsiteDownloaderProgram/Program.cs
+++ WebsiteDownloaderProgram/WebsiteDownloaderProgram/Program.cs
@@ -4,9 +4,29 @@ namespace WebsiteDownloaderProgram
 {
     class Program
     {
+        private static string webSite = "www.tretton37.com";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the develop branch");
+            try
+            {
+                if (args?.Length > 0)
+                {
+                    webSite = args[0];
+                }
+
+                Console.WriteLine("Starting program");
+
+                new Downloader().Run(webSite);
+
+                Console.WriteLine("Ending program");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went wrong! (The program will now end) \nTechnical message: {e.Message}");
+                Console.ReadLine();
+            }
         }
     }
 }
