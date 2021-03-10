b3bad2fa0f63f159c6bcaf82f4a6a10aea66e89d
Martin Nordkvist <martin.nordkvist@sogeti.com>
2021-03-10 20:54:39 +0100

    Creating develop branch

--- WebsiteDownloaderProgram/WebsiteDownloaderProgram/Program.cs
+++ WebsiteDownloaderProgram/WebsiteDownloaderProgram/Program.cs
@@ -6,7 +6,7 @@ namespace WebsiteDownloaderProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the develop branch");
         }
     }
 }
